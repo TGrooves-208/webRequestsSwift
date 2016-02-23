a27c75a19f3bcb418794409bf7e26a80ef582add
TGrooves-208 <tgrooves.208@gmail.com>
2016-02-22 22:41:18 -0700

    intial commit of web requests

--- webrequest-test/ViewController.swift
+++ webrequest-test/ViewController.swift
@@ -7,19 +7,58 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var container: UIView! 
+    var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        webView = WKWebView()
+        container.addSubview(webView)
+        //Debugging the rendering of the page on the iPhone and the 
+        //Image View itself
+        //print(container.bounds.width)
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        let frame = CGRectMake(0,0, container.bounds.width, container.bounds.height)
+        webView.frame = frame
+        
+        loadRequest("https://developer.apple.com/swift/blog/")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func loadRequest(urlStr: String) {
+        
+        let url = NSURL(string: urlStr)!
+        let request = NSURLRequest(URL: url)
+        webView.loadRequest(request)
 
+
+    }
+    
+    
+    @IBAction func loadSteak(sender: AnyObject) {
+        loadRequest("http://www.allenbrothers.com/wet-aged-beef.html")
+    }
+
+    @IBAction func loadGossip(sender: AnyObject) {
+        loadRequest("http://www.tmz.com/")
+    }
+    
+    @IBAction func loadSwift(sender: AnyObject) {
+        loadRequest("https://developer.apple.com/swift/blog/")
+        
+    }
 
 }
 
